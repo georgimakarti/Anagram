cb07ed3ede4fb4166b7ad650522ee86cfefdc21f
Georgi Georgiev <75014714+georgimakarti@users.noreply.github.com>
2022-06-10 15:23:05 +0300

    Add files via upload

---
+++ matrix test/matrix test/Program.cs
@@ -0,0 +1,44 @@
+﻿string str = "goergi";
+string strrr = "georgi";
+
+//SortingString(str);
+//SortingString(strrr);
+CheckIfAnagram(str, strrr);
+
+
+
+void CheckIfAnagram(string str, string strrr)
+{
+
+    string refer = Convert.ToString(SortingString(str))!;
+    string referr = Convert.ToString(SortingString(strrr))!;
+
+    Console.WriteLine("{0} {1}", refer, referr);
+
+    for (int i = 0; i < refer.Length; i++)
+    {
+
+       if (refer[i] != referr[i])
+           {
+               Console.WriteLine("The String is not Anagram {0} {1}", refer[i], referr[i]);
+               return;
+           }
+        Console.WriteLine("{0} {1}", refer[i], referr[i]);
+    }
+    Console.WriteLine("The string is anagrama");
+}
+
+List<char> SortingString(string str)
+{
+    List<char> sortedList = new List<char>();
+    sortedList.AddRange(str.ToLower().ToCharArray());
+    sortedList.Sort();
+
+    foreach (var i in sortedList)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+    return sortedList;
+}
+
